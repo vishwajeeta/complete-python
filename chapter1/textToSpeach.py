5b20225f73087a07dbd148eada46ff870a77a58a
vishwajeeta <gs699542@gmail.com>
2024-06-17 13:22:38 +0530

    adding random module and user input

--- chapter1/textToSpeach.py
+++ chapter1/textToSpeach.py
@@ -1,5 +1,15 @@
 import pyttsx3
+import random
+
+a=["master","vishwa","boss"]
+
+user=random.choice(a)
 
 engine=pyttsx3.init()
-engine.say("hello i hope you have a good day.")
+engine.say(f"hello {user} hope you have a good day.")
+
+engine.runAndWait()
+
+user_input=input("what you want to listen:")
+engine.say(user_input)
 engine.runAndWait()
