560bec2b2216324bb940510179e090115a5661f0
vishwajeeta <gs699542@gmail.com>
2024-06-20 13:35:28 +0530

    updated

---
+++ chapter3/002_list.py
@@ -0,0 +1,20 @@
+MyList=["vishwa",101,0.59]
+print(MyList)
+MyList.append("buyy")
+print(MyList)
+
+#creating new list
+omg=[3,9,7,2,6]
+print(omg)
+
+#sorting
+omg.sort()
+print(omg)
+omg.reverse()
+print(omg)
+
+#Methods
+# push
+# pop
+# insert
+# remove
--- chapter3/01_List.py
+++ chapter3/01_List.py
@@ -3,3 +3,7 @@ print(MyList)
 print(MyList[0])
 MyList[0]=10
 print(MyList)
+omg=[[1,2,3],[3,2,1],[4,5,6],[6,5,4]]
+print("New string=",omg)
+print(omg[0],omg[2])
+print(omg[0][2],omg[2][1:])
