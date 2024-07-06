36539ef5b5dd090a6ef5b60f985759f56f19a522
vishwajeeta <gs699542@gmail.com>
2024-07-06 10:43:49 +0530

    creating games

--- project1/guess_ludo_dies_number.py
+++ project1/guess_ludo_dies_number.py
@@ -1,10 +1,14 @@
 import random
+
 computer=random.choices([1,2,3,4,5,6])
 you=int(input("Enter your choice: "))
-if(you>=6 or you<=0):
+computer=computer[0]
+print(f"computer chosed:{computer}\nyou chosed:{you}")
+if(you>6 or you<=0):
     print("pleace choose above 0 and below 7")
 else:
     if(computer==you):
-        print("You win")
+        print("congrates You win!")
+        print("Password is: ***************** ")
     else:
         print("you lose!")
