23ae63266a72939289ed1fe6b1691458822298b2
vishwajeeta <gs699542@gmail.com>
2024-07-15 01:56:20 +0530

    sample of oops

---
+++ object oriented programming/sample1.py
@@ -0,0 +1,12 @@
+class vishwa:
+    def home():
+        print("i am in home")
+    def collage(self):
+        print("i am at collage")
+        print(self)
+vishwa1=vishwa.home()
+
+omg=vishwa()
+omg=omg.collage()
+
+vishwa().collage()
