88c8734f68aa1bebd97eb9f00fd1b4544bf207b3
vishwajeeta <gs699542@gmail.com>
2024-07-16 14:18:06 +0530

    unsucessful access

--- web socket/experiment1.py
+++ web socket/experiment1.py
@@ -1,8 +1,11 @@
 import socket
 
 mysock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-mysock.connect(('data.pr4e.org', 80))
-cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
+socket.getaddrinfo('localhost',3000)
+# mysock.connect(('data.pr4e.org', 80))
+mysock.connect(('localhost:3000', 80))
+# cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
+cmd = 'GET http://127.0.0.1:3000/ HTTP/1.0\r\n\r\n'.encode()
 mysock.send(cmd)
 while True:
     data = mysock.recv(512)
