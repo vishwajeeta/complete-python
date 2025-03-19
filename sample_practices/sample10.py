425390e04e68753ebbac0e166bee588f170e17e4
vishwajeeta <gs699542@gmail.com>
2025-03-19 20:18:47 +0530

    updated

---
+++ sample_practices/sample10.py
@@ -0,0 +1,6 @@
+a=10
+b=199
+if a != b:
+    print("correct")
+else:
+    print("incorrect")
---
+++ sample_practices/sample8.py
@@ -0,0 +1,23 @@
+a=39
+b=39
+print(a)
+a =56
+b =35
+print(a,b)
+a=56+3
+b=76+9
+print(a)
+
+def subtraction():
+    return 3-1
+print(subtraction())
+
+def sakshi():
+    return 32+3
+sakshi()
+print("sakshi")
+
+def ftgyhju():
+    return ('sakshi')
+print('sakshi,y')
+
