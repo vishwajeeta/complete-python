654c61d66faa0624b43f258848a65931ece15bbb
vishwajeeta <gs699542@gmail.com>
2025-03-21 17:29:37 +0530

    tution

---
+++ sample_practices/prom.py
@@ -0,0 +1,13 @@
+print('''become a hub of industries the indian craftsmen become unemployed primarily the textile and wool industries became sick and people lost their 
+jobs the cottage industries too suddered a death blow the british master imposed a heavy tax on sale of indian goods in england because of the 
+zamindari system the landlords acting as commission was appointed and all the gifted lands many financial problems this too led revolt of
+farmers against the government
+
+administrative causes 
+the british master brought into force new civil and criminal laws these were applicable only to indians english become the the language of the court 
+most of the time the british ''')
+
+a=23
+b=34
+c=a+b
+print(c)
